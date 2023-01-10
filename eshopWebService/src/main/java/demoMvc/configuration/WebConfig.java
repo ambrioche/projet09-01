0a4c66067c16fb45f9dced366870070bc1cef590
kennyguiroy123 <kenny.guiroy@gmail.com>
2023-01-10 10:52:27 +0100

    Update groupID

--- eshopWebService/src/main/java/demoMvc/configuration/WebConfig.java
+++ eshopWebService/src/main/java/demoMvc/configuration/WebConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
-import exoFormation.config.JpaConfig;
+import formationAlten.config.JpaConfig;
+
 
 @Configuration
 @EnableWebMvc
